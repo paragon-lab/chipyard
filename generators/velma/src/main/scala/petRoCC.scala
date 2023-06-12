1b34b202b7c50fed8992d9d6c81027109f472a4d
Connor Selna <connorselna2021@u.northwestern.edu>
2023-06-12 03:00:30 -0500

    decoupled interfaces traverse modules in annoying ways. trait inclusions fixed.

--- generators/velma/src/main/scala/petRoCC.scala
+++ generators/velma/src/main/scala/petRoCC.scala
@@ -1,21 +1,39 @@
 /* petRoCC -- Author: Connor Selna
  * Acknowledgements: Karl Hallsby is cool, Berkeley Architecture Research is also cool
  */
+package velma
+
+import chisel3._
+import chisel3.util._
+import freechips.rocketchip.tile._
+import freechips.rocketchip.diplomacy._
+import freechips.rocketchip.config._
+import freechips.rocketchip.rocket._
+import freechips.rocketchip.tilelink._
+import chisel3.experimental.ChiselEnum
+import freechips.rocketchip.config.Parameters
+//import freechips.rocketchip.tile.{XLen, CoreModule, RoCCCommand}
+//import freechips.rocketchip.rocket.constants.MemoryOpConstants
+//import freechips.rocketchip.rocket.MStatus
+//import freechips.rocketchip.rocket.{HellaCacheReq, HellaCacheResp}
+import freechips.rocketchip.util.EnhancedChisel3Assign
 
 
 
-class petRoCCControlUnit extends Module {
+class petRoCCControlUnit extends Module
+  with HasCoreParameters
+  {
   val io = IO(new Bundle{
     val cmd = Input(new RoCCCommand)
     val do_read = Output(Bool())
-    val read_addr = Output(UINT(width = coreMaxAddrBits)) 
-    val rd = Output(UINT(5.W))
+    val read_addr = Output(UInt(width = coreMaxAddrBits.W)) 
+    val rd = Output(UInt(5.W))
   })
 
 
 ////////////////////////////////// Receive and Buffer Command //////////////////////////////////////////////////////
 //enable register updating to get cmd
-  val cmd_regEn = io.cmd.valid //also going to want to AND this with inversions of our busy signals for to be safe
+  val cmd_regEn = io.cmd.fire() //also going to want to AND this with inversions of our busy signals for to be safe
   /*store cmd, this basically works as our storage for all the control signals, since the control is determined
     mostly combinationally--will still need to store our read data, though.*/
   val cmd = RegEnable(io.cmd, cmd_regEn)
@@ -41,32 +59,30 @@ class petRoCCControlUnit extends Module {
   val rs2d = cmd.bits.rs2 //the stored value in rs2
 
   //feelin real dumb here: if we get a valid RoCCCommand, just straight up do the read.
-  when (cmd.valid){
-    io.do_read := true.B}
-  .otherwise{
-    io.do_read := false.B}
-
+//  when (cmd.valid){
+//    io.do_read := true.B}
+//  .otherwise{
+//    io.do_read := false.B}
+  io.do_read := true.B 
 
   io.read_addr := rs1d
   io.rd := rd
-  
-// CHECK OUT OBJECT OPCODESET, LINE 400ISH, LAZYROCC.SCALA FOR FREE ENCODINGS 
 
-
-  })
-}
+  }
 
 
 
 
-class petRoCCMemRequestMaker extends Module {
+
+class petRoCCMemRequestMaker extends Module 
+      with HasCoreParameters{
   val io = IO(new Bundle {
     //inputs
-    val read_addr = Input(UINT(width = coreMaxAddrBits))
+    val read_addr = Input(UInt(width = coreMaxAddrBits.W))
     val do_read = Input(Bool())
-    val memop_size = Input(UINT) //2^memop_size tells us the number of BYTES in our operation
+    val memop_size = Input(UInt()) //2^memop_size tells us the number of BYTES in our operation
     //outputs
-    val mreq = new Output(HellaCacheReq)
+    val mreq = Output(new HellaCacheReq)
     val req_sent = Output(Bool())
     val req_busy = Output(Bool())
   })
@@ -74,34 +90,37 @@ class petRoCCMemRequestMaker extends Module {
   // ASSEMBLE MEMORY REQUEST 
   val mreq = new HellaCacheReq
   io.mreq := mreq
-  mreq.bits.addr := rs1d
-  mreq.bits.tag :=  3.U(2.W)//unsigned integer with width of 7 and value 3. If signed, would be -1.
+  mreq.addr := rs1d
+  mreq.tag :=  3.U(2.W)//unsigned integer with width of 7 and value 3. If signed, would be -1.
   //mreq.bits.idx : //leaving this guy unused for now, the examples also don't use it and i'll just pray :)
-  mreq.bits.cmd := M_XRD //apparently how we issue a read; a store is issued as M_XWR
-  mreq.bits.size := log2Ceil(8).U //size of data we want--log_2(8)=3
-  mreq.bits.data := 0.U //doing a read for now.
-  mreq.bits.phys := false.B
+  mreq.cmd := M_XRD //apparently how we issue a read; a store is issued as M_XWR
+  mreq.size := log2Ceil(8).U //size of data we want--log_2(8)=3
+  mreq.data := 0.U //doing a read for now.
+  mreq.phys := false.B
   //mreq.bits.dprv := cmd.bits.status.dprv //THIS LINE NEEDS FIXING DUE TO MODULARIZATION
   //mreq.bits.dv := cmd.bits.status.dv //THIS LINE NEEDS FIXING DUE TO MODULARIZATION
   mreq.valid := cmd.valid// LATTER PORTIONS COMMENTED FOR DUMB VERSON && (opcode == "b0000001".U) 
-  mreq.ready := mreq.valid //not doing anything fancy rn, so as soon as our command is valid...
-
+  
+  //mreq.ready := mreq.valid //not doing anything fancy rn, so as soon as our command is valid...
+        // -> so there apparently isn't an mreq.ready -- the readys come from the Decoupled subclasses
 
   val req_busy = Bool()
-  when (mreq.fire()){ 
+  when (mreq.fire){ 
     req_busy := true.B} //is the request in flight? we're busy.
   .otherwise{
     req_busy := false.B
     }
 }
 
-class petRoCCMemResponseTaker extends Module {
+class petRoCCMemResponseTaker extends Module 
+  with HasCoreParameters
+  {
   val io = IO(new Bundle {
     //inputs
     val mresp = Input(new HellaCacheResp)
     //outputs: data, has_data, addr, ready/valid of some kind :)
     val valid_read = Output(Bool())
-    val read_data = Output(UINT(xLen.W))
+    val read_data = Output(UInt(xLen.W))
     val mresp_busy = Output(Bool())
   })
 
@@ -113,7 +132,7 @@ class petRoCCMemResponseTaker extends Module {
   val read_data = RegEnable(mresp.bits.data, valid_read)//buffer the data when it comes in!
   val mresp_busy = Bool()
 
-  when mresp.fire(){
+  when (mresp.fire()){
     mresp_busy := true.B}
   .otherwise{
     mresp_busy := false.B}
@@ -125,7 +144,9 @@ class petRoCCMemResponseTaker extends Module {
 
 }
 
-class petRoCCCoreResponseMaker extends Module {
+class petRoCCCoreResponseMaker extends Module
+  with HasCoreParameters
+  {
   val io = IO(new Bundle {
     //inputs
     val rd = Input(Bits(5.W))
@@ -152,7 +173,9 @@ class petRoCCCoreResponseMaker extends Module {
   io.busy := io.mresp_busy || io.req_busy 
 }
 
-class petRoCCSharpieMouth extends Module {
+class petRoCCSharpieMouth extends Module 
+  with HasCoreParameters
+  {
   val io = IO(new Bundle {
     val data = Input(Bits(xLen.W)) //was width = coreDataBits
   //  val doit = Input(Bool())
@@ -163,13 +186,13 @@ class petRoCCSharpieMouth extends Module {
 }
 
 
-class petRoCC(opcodes: OpcodeSet)(implicit p: parameters) extends LazyRoCC(opcodes = opcodes, nPTWPorts = 1){
+class petRoCC(opcodes: OpcodeSet)(implicit p: Parameters) extends LazyRoCC(opcodes = opcodes, nPTWPorts = 1){
   override lazy val module = new petRoCCModule(this)
 }
 
 
 
-class petRoCCModule(outer: petRoCC) extends LazyRoCCModuleImp(outer) {
+class petRoCCModule(outer: petRoCC)(implicit p: Parameters) extends LazyRoCCModuleImp(outer) {
 /*io here is implied to be a RoCCIO (extends RoCCCoreIO) type, maintain for now.
 RoCCIO signals: 
 val ptw = Vec(nPTWPorts, new TLBPTWIO)
@@ -186,12 +209,19 @@ val exception = Input(Bool()) //input to handle exceptions
 */
 
 
+////////////////////////////////// Receive and Buffer Command //////////////////////////////////////////////////////
+//enable register updating to get cmd
+  val cmd_regEn = io.cmd.fire() //also going to want to AND this with inversions of our busy signals for to be safe
+  /*store cmd, this basically works as our storage for all the control signals, since the control is determined
+    mostly combinationally--will still need to store our read data, though.*/
+  val cmd = RegEnable(io.cmd, cmd_regEn)
+
 /////////////////////////////////////////////////////////////////////
 /////////////////// Control Handling//////////////////
 ////////////////////////////////////////
   val ctrl = Module(new petRoCCControlUnit)
   //inputs
-  ctrl.io.cmd := io.cmd
+  ctrl.io.cmd := cmd
   //outputs
   val do_read = ctrl.io.do_read
   val read_addr = ctrl.io.read_addr
